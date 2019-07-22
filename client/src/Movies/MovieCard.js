c685ff8139de21cd3c3b12d3014ec4ec918939c7
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-07-22 14:22:04 -0700

    Trying to figure out why no console log is showing

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MovieCard = props => {
-  return;
+  return(
+    <Link to='/movie'>
+      <MovieCard></MovieCard>
+    </Link>
+  )
 };
 
 export default MovieCard;
